a3fc9d9e47a4e38944f17f3b77f1d19618e91bcf
tibilodondev <tibilodondev@gmail.com>
2023-10-26 15:49:03 +0200

    more prisma errors

--- prisma/migrations/20230912153625_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `logs` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(250) NOT NULL,
-    `file_name` VARCHAR(50) NOT NULL,
-    `line_number` INTEGER NOT NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
